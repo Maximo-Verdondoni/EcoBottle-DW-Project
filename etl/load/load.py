78b7de9a2bfaa6448de49f0fbe6c3a8e0cd7f759
Maximo-Verdondoni <avryux04@gmail.com>
2025-10-21 22:44:31 -0300

    feat(dim_product): se agregó dim_product desnormalizando product_category

--- etl/load/load.py
+++ etl/load/load.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from etl.extract.extract import extract_all
 from etl.transform.build_dim_calendar import build_dim_calendar
 from etl.transform.build_dim_customer import build_dim_customer
+from etl.transform.build_dim_product import build_dim_product
 #from etl.transform.build_dim_product import build as build_dim_product
 
 OUTPUT_PATH = Path("warehouse")  #A donde apunta el pipeline
@@ -14,6 +15,7 @@ def run_pipeline():
 
     df_dim_calendar = build_dim_calendar(OUTPUT_PATH, "2023-01-01", "2026-12-31")
     df_dim_customer = build_dim_customer(data, OUTPUT_PATH)
+    df_dim_product = build_dim_product(data, OUTPUT_PATH)
     #df_dim_products = build_dim_product(data, OUTPUT_PATH)
 
     print("✅ Pipeline completado. Archivos guardados en warehouse/")
---
+++ etl/transform/build_dim_product.py
@@ -0,0 +1,53 @@
+# etl/transform/build_dim_products.py
+import pandas as pd
+
+def build_dim_product(data: dict, output_path):
+    """
+    Construye la dimensión de productos desnormalizando categorías y productos.
+    Guarda el resultado en warehouse/dim/dim_products.csv
+    """
+    categories = data["product_category"].copy()
+    products = data["product"].copy()
+
+    # Limpiar y convertir parent_id
+    categories['parent_id'] = categories['parent_id'].replace('', None)
+    categories['parent_id'] = pd.to_numeric(categories['parent_id'], errors='coerce')
+
+    # Preparar categorías padre
+    parent_categories = categories[["category_id", "name"]].copy()
+    parent_categories = parent_categories.rename(
+        columns={"category_id": "parent_id", "name": "parent_category_name"}
+    )
+
+    # Unir categorías con sus padres
+    categories_enriched = pd.merge(
+        categories,
+        parent_categories,
+        on="parent_id",
+        how="left",
+        suffixes=('', '_parent')
+    )
+
+    # Renombrar columnas
+    categories_enriched = categories_enriched.rename(
+        columns={"name": "category_name"}
+    )
+
+    # Combinar productos con categorías enriquecidas
+    dim_products = pd.merge(
+        products,
+        categories_enriched[["category_id", "category_name", "parent_category_name"]],
+        on="category_id",
+        how="left"
+    )
+
+    # Eliminar category_id y reordenar columnas si es necesario
+    dim_products = dim_products.drop("category_id", axis=1)
+
+    # salida en warehouse/dim
+    file_path = output_path / "dim" / "dim_products.csv"
+    file_path.parent.mkdir(parents=True, exist_ok=True)  # aseguro que exista la carpeta
+    dim_products.to_csv(file_path, index=False)
+    
+    print(f"✅ dim_products guardado en {file_path}")
+    return dim_products
