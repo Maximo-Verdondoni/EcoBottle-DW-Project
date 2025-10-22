77dcdc6b6d0c7d9748ccce7d21783e41679254ef
Maximo-Verdondoni <avryux04@gmail.com>
2025-10-21 23:14:35 -0300

    feat(dim_address): se agrego dim_address desnormalizando con province

--- etl/load/load.py
+++ etl/load/load.py
@@ -4,6 +4,7 @@ from etl.extract.extract import extract_all
 from etl.transform.build_dim_calendar import build_dim_calendar
 from etl.transform.build_dim_customer import build_dim_customer
 from etl.transform.build_dim_product import build_dim_product
+from etl.transform.build_dim_address import build_dim_address
 #from etl.transform.build_dim_product import build as build_dim_product
 
 OUTPUT_PATH = Path("warehouse")  #A donde apunta el pipeline
@@ -16,6 +17,7 @@ def run_pipeline():
     df_dim_calendar = build_dim_calendar(OUTPUT_PATH, "2023-01-01", "2026-12-31")
     df_dim_customer = build_dim_customer(data, OUTPUT_PATH)
     df_dim_product = build_dim_product(data, OUTPUT_PATH)
+    df_dim_address = build_dim_address(data, OUTPUT_PATH)
     #df_dim_products = build_dim_product(data, OUTPUT_PATH)
 
     print("✅ Pipeline completado. Archivos guardados en warehouse/")
---
+++ etl/transform/build_dim_address.py
@@ -0,0 +1,44 @@
+# etl/transform/build_dim_address.py
+import pandas as pd
+
+def build_dim_address(data: dict, output_path):
+    """
+    Construye la dimensión de dirección desnormalizando con provincia
+    Guarda el resultado en warehouse/dim/dim_customer.csv
+    """
+    addresses = data["address"].copy()
+    provinces = data["province"].copy()
+
+    # Unir direcciones con provincias
+    dim_address = pd.merge(
+        addresses,
+        provinces[['province_id', 'name', 'code']].rename(
+            columns={'name': 'province_name', 'code': 'province_code'}
+        ),
+        on='province_id',
+        how='left'
+    )
+
+    # Agregar tipo de dirección basado en el ID (opcional)
+    dim_address['address_type'] = dim_address['address_id'].apply(
+        lambda x: 'store' if x <= 4 else 'customer'
+    )
+
+    # CREAR SURROGATE KEY 
+    dim_address['id'] = range(1, len(dim_address) + 1)
+
+    # Reordenar columnas - ID surrogada primero, mantener address_id como business key
+    dim_address = dim_address[[
+        'id',                           # Surrogate key
+        'line1', 'line2', 'city', 
+        'province_name', 'province_code',
+        'postal_code', 'country_code', 'address_type', 'created_at'
+    ]]
+
+    # salida en warehouse/dim
+    file_path = output_path / "dim" / "dim_address.csv"
+    file_path.parent.mkdir(parents=True, exist_ok=True)  # aseguro que exista la carpeta
+    dim_address.to_csv(file_path, index=False)
+    
+    print(f"✅ dim_address guardado en {file_path}")
+    return dim_address
