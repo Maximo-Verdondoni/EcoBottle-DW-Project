8a21f7048b94b5468de8a0bd5e58a14bf772ec47
Maximo Verdondoni <avryux04@gmail.com>
2025-10-22 17:57:19 -0300

    fix(dim_channel): se agrego la clave natural channel_key

--- etl/transform/build_dim_channel.py
+++ etl/transform/build_dim_channel.py
@@ -8,7 +8,12 @@ def build_dim_channel(data: dict, output_path):
     """
     dim_channel = data["channel"].copy()
 
-    dim_channel = dim_channel.rename(columns={"channel_id": "id"})
+    dim_channel = dim_channel.rename(columns={"channel_id": "channel_key"})
+    dim_channel['id'] = range(1, len(dim_channel) + 1)
+
+    dim_channel = dim_channel[[
+        'id', 'channel_key', 'code', 'name'
+    ]]
 
     # salida en warehouse/dim
     file_path = output_path / "dim" / "dim_channel.csv"
