297de43b705f4bf11bb780c0f4baec6e29838302
Maximo Verdondoni <avryux04@gmail.com>
2025-10-22 19:51:09 -0300

    fix(fact_shipment): se simplifico el transform para el hecho

--- etl/transform/build_fact_shipment.py
+++ etl/transform/build_fact_shipment.py
@@ -7,9 +7,7 @@ def build_fact_shipment(data,dim_calendar,dim_customer,dim_channel,dim_address, 
     """
     fact_shipment = data["shipment"].copy()
     sales_order = data["sales_order"].copy()
-    customers_raw = data["customer"].copy()
-    addresses_raw = data["address"].copy()
-    channels_raw = data["channel"].copy()
+
     dim_calendar = dim_calendar.copy()
     dim_customer = dim_customer.copy()
     dim_channel = dim_channel.copy()
@@ -25,69 +23,38 @@ def build_fact_shipment(data,dim_calendar,dim_customer,dim_channel,dim_address, 
         how='left'
     )
 
-    # PASO 2: Crear mapeo de customer_id business key a surrogate key
-    # Necesitamos los datos crudos de customer para tener el customer_id original
-    customer_map = customers_raw[['customer_id']].reset_index(drop=True)
-    customer_map = customer_map.merge(
-        dim_customer.reset_index()[['id']], 
-        left_index=True, 
-        right_index=True
-    ).rename(columns={'id': 'customer_sk'})
-
-    # PASO 3: Obtener la surrogate key de dim_customer
+    # DIM CUSTOMER
     fact_shipment = pd.merge(
         fact_shipment,
-        customer_map,
-        on='customer_id',
-        how='left'
-    )
-    # borramos la id original y nos quedamos con la surrogada
-    fact_shipment = fact_shipment.drop(columns=["customer_id"])
-    fact_shipment = fact_shipment.rename(columns={'customer_sk': 'customer_id'})
+        dim_customer,
+        left_on='customer_id',
+        right_on='customer_key',
+        how='left',
+        suffixes=('_payment', '_customer')
+    ).drop(columns=['customer_id','customer_key'])
+    fact_shipment = fact_shipment.rename(columns={'id': 'customer_id'})
 
 
     ## DIM_ADDRESS (shipping_address)
-    # PASO 1: Crear mapeo de shipping_address_id business key a surrogate key
-    # Necesitamos los datos crudos de customer para tener el customer_id original
-    address_map = addresses_raw[['address_id']].reset_index(drop=True)
-    address_map = address_map.merge(
-        dim_address.reset_index()[['id']], 
-        left_index=True, 
-        right_index=True
-    ).rename(columns={'id': 'shipping_address_sk'})
-    # PASO 2: Obtener la surrogate key de dim_address
     fact_shipment = pd.merge(
         fact_shipment,
-        address_map,
+        dim_address,
         left_on='shipping_address_id',
-        right_on= 'address_id',
+        right_on='address_key',
         how='left'
-    )
-    # borramos la id original y nos quedamos con la surrogada
-    fact_shipment = fact_shipment.drop(columns=["address_id", "shipping_address_id"]) #Borramos las ids originales
-    fact_shipment = fact_shipment.rename(columns={'shipping_address_sk': 'shipping_address_id'})
+    ).drop(columns=['shipping_address_id','address_key'])
+    fact_shipment = fact_shipment.rename(columns={'id': 'shipping_address_id'})
+    fact_shipment['shipping_address_id'] = fact_shipment['shipping_address_id'].astype('Int64') #algunos pagos no tienen billing
 
     # DIM_CHANNEL
-    #buscamos channel_id en order_sales, buscamos su surrogada en dim_channel
-    # y nos traemos esa columna en la fact
-    # PASO 1: Crear mapeo de channel_id business key a surrogate key
-    # Necesitamos los datos crudos de channel para tener el channel_id original
-    channel_map = channels_raw[['channel_id']].reset_index(drop=True)
-    channel_map = channel_map.merge(
-        dim_channel.reset_index()[['id']], 
-        left_index=True, 
-        right_index=True
-    ).rename(columns={'id': 'channel_sk'})
-    # PASO 2: Obtener la surrogate key de dim_channel
     fact_shipment = pd.merge(
         fact_shipment,
-        channel_map,
-        on="channel_id",
+        dim_channel,
+        left_on='channel_id',
+        right_on='channel_key',
         how='left'
-    )
-    # borramos la id original y nos quedamos con la surrogada
-    fact_shipment = fact_shipment.drop(columns=["channel_id"])
-    fact_shipment = fact_shipment.rename(columns={'channel_sk': 'channel_id'})
+    ).drop(columns=['channel_id', 'channel_key'])
+    fact_shipment = fact_shipment.rename(columns={'id': 'channel_id'})
 
     #separamos shipped_at y delivered_at en fecha y hora
     fact_shipment['shipped_at'] = pd.to_datetime(fact_shipment['shipped_at'])
